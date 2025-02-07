0edbe57901ba9a40ebcba9996840e2c45947e0f7
David-F-P <davidbv645@gmail.com>
2025-02-07 00:52:59 -0600

    actualizacion inventario

--- src/app/pages/inventario/inventario.page.ts
+++ src/app/pages/inventario/inventario.page.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
 import { InventarioService } from 'src/app/services/inventario.service';
 import { Prenda } from 'src/app/models/prenda';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-inventario',
   templateUrl: './inventario.page.html',
   styleUrls: ['./inventario.page.scss'],
   standalone: false,
-
 })
 export class InventarioPage {
   inventario: Prenda[] = [];
   valorTotal: number = 0;
   disenador: string = '';
 
-  constructor(private inventarioService: InventarioService) {}
+  constructor(private inventarioService: InventarioService, private navCtrl: NavController) {}
 
   ngOnInit() {
     this.inventario = this.inventarioService.getInventario();
@@ -27,5 +27,12 @@ export class InventarioPage {
 
   buscar() {
     this.inventario = this.inventarioService.buscarPorDisenador(this.disenador);
+  }
+
+  /**
+   * Redirige a la página de Home.
+   */
+  irAHome() {
+    this.navCtrl.navigateBack('/home');
   }
 }
